df840c4f569701a240190a758dfce6594f7fc2c6
Kevin Grandemange <grandemange.kevin@gmail.com>
2020-12-12 19:51:53 +0100

    Add a commandline flag to take the token of the discord bot

--- main.ts
+++ main.ts
@@ -1,2 +1,24 @@
-let message : string = "hello";
-console.log(message);
+import { CommandLineStringParameter, CommandLineParser } from "@rushstack/ts-command-line";
+
+export class MainCommandLine extends CommandLineParser {
+    public token: CommandLineStringParameter;
+    public constructor() {
+        super({
+            toolFilename: "main",
+            toolDescription: 'commandline'
+        });
+    }
+
+    protected onDefineParameters(): void {
+        this.token = this.defineStringParameter({
+            argumentName: "TOKEN",
+            parameterLongName: "--token",
+            parameterShortName: "-t",
+            description:"Token to access the discord"
+        });
+    }
+}
+const commandLine = new MainCommandLine();
+commandLine.execute().then(() => {
+    console.log("The token is " + commandLine.getStringParameter("--token").value);
+});
